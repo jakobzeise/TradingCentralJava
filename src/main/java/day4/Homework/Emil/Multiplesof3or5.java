d015a6dc740e2ed52361aa542a6068f991a3e9e0
Emil <emil.draganescu@tradingcentral.com>
2023-08-21 21:11:30 +0300

    Homework 4: Emil

---
+++ src/main/java/day4/Homework/Emil/Multiplesof3or5.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+  public int solution(int number) {
+    //TODO: Code stuff, below
+    
+    int sum = 0;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+  }
+}
