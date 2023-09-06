b8f8931b250bf73b1203130849fd83b14115e566
Muaammar ALI <muaammarkuzman@yahoo.com>
2023-09-06 15:51:12 +0200

    Add homework of day5

---
+++ src/main/java/day5/Muaammar/BasicLibrarySystem.java
@@ -0,0 +1,42 @@
+package day5homework.java.Muaammar;
+
+public class BasicLibrarySystem {
+    public static void main(String[] args) {
+
+        Book[] books = {
+                new Book("Book1_title", "Riad Numa", "ABCD12345", 6),
+                new Book("Book2_title", "Francois Reit", "FGHIJ67890", 5),
+                new Book("Book3", "Sophia John", "bvfd67890", 3)
+        };
+
+        LibraryMember[] members = {
+                new LibraryMember("ID1234", "John", "Joao"),
+                new LibraryMember("XX56789", "Alex", "Bob")
+        };
+
+
+        for (LibraryMember member : members) {
+            for (Book book : books) {
+                member.borrowBook(book);
+                book.checkoutBook();
+            }
+        }
+
+        for (LibraryMember member : members) {
+            System.out.println(member.getFirstName() + " borrowed following books:");
+            for (Book book : books) {
+                if (member.getBorrowedBooks().contains(book)) {
+                    System.out.println(book.getTitle());
+                }
+            }
+            System.out.println();
+        }
+
+        for (LibraryMember member : members) {
+            for (Book book : books) {
+                member.returnBook(book);
+                book.returnBook();
+            }
+        }
+    }
+}
