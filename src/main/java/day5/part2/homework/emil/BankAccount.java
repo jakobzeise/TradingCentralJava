af72e721584d44084c1b3c8fd802cb4d9074d46e
Jakob Zeise <jakobzeise8@gmail.com>
2023-09-07 22:35:47 +0200

    OOP

---
+++ src/main/java/day5/part2/homework/emil/BankAccount.java
@@ -0,0 +1,27 @@
+package day5.part2.homework.emil;
+
+public class BankAccount {
+    int accountNumber;
+    double balance;
+
+    public BankAccount(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ src/main/java/day5/part2/homework/emil/Main.java
@@ -0,0 +1,17 @@
+package day5.part2.homework.emil;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(666, 1000.0d);
+
+        System.out.println("Initial balance: " + account.getBalance());
+
+        account.deposit(500.0d);
+        System.out.println("Balance after deposit: " + account.getBalance());
+
+        account.withdraw(1000.0d);
+        System.out.println("Balance after withdrawal: " + account.getBalance());
+
+        account.withdraw(1500.0); // This will display an error message
+    }
+}
